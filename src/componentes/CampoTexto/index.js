2f37d63c0ffd9d9eb0346ddb4dd5b56cb900d9d3
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-05-27 10:02:19 -0600

    Manejo de estado de los inputs para su control y obtención de valores para envíarlos posteriormente a algún servicio o procesamiento deseado.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,16 @@
+import { useState } from "react"
 import "./campoTexto.css"
 
 const CampoTexto = (props) => {
+
+    const manejarCambio = (e) => {
+        props.actualizarValor(e.target.value);
+    }
+
     const placeHolderMood = `${props.placeholder}...`
     return <div className="campo-texto">
         <label >{props.titulo}</label>
-        <input placeholder={placeHolderMood} required={props.required}/>
+        <input placeholder={placeHolderMood} required={props.required} value={props.valor} onChange={manejarCambio}/>
     </div>
 }
 
--- src/componentes/Formulario/formulario.js
+++ src/componentes/Formulario/formulario.js
@@ -1,19 +1,29 @@
+import { useState } from "react"
 import "./formulario.css"
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaDeOpciones"
 const Formulario = () =>{
 
+    const [nombre,actualizarNombre] = useState("");
+    const [puesto,actualizarPuesto] = useState("");
+    const [urlDeFoto,actualizarUrlDeFoto] = useState("");
+
     const manejarEnvioDeFormulario = (event) => {{
         event.preventDefault();
-        console.log("hola");
+        let _data = {
+            nombre: nombre,
+            puesto: puesto,
+            urlDeFoto : urlDeFoto
+        }
+        console.log(_data);
     }}
 
     return <section className="formulario">
         <form onSubmit={manejarEnvioDeFormulario}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required/>
-            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required/>
-            <CampoTexto titulo="Foto" placeholder="Ingresar url de su foto de perfil" required/>
+            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required valor={nombre} actualizarValor={actualizarNombre}/>
+            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required valor={puesto} actualizarValor={actualizarPuesto}/>
+            <CampoTexto titulo="Foto" placeholder="Ingresar url de su foto de perfil" required valor={urlDeFoto} actualizarValor={actualizarUrlDeFoto}/>
             <ListaOpciones/>
             <button type="submit">Crear Colaborador</button>
         </form>
